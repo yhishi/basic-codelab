c2e46cdcb6074ab14dd47a17043513fa3ff0ba2d
yhishi <ev422119.s.okayama@gmail.com>
2023-10-29 22:26:05 +0900

    Composableの中ではrememberAPIを使って、状態を保持する

--- app/src/main/java/com/example/basic/codelab/MainActivity.kt
+++ app/src/main/java/com/example/basic/codelab/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,6 +34,7 @@ class MainActivity : ComponentActivity() {
 private fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")) {
     Column(modifier = Modifier.padding(4.dp)) {
         names.forEach { name ->
+
             Greeting(name)
         }
     }
@@ -39,6 +42,8 @@ private fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("W
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) }
+
     // A surface container using the 'background' color from the theme
     Surface(modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp), color = MaterialTheme.colorScheme.primary) {
         Row(modifier = Modifier.padding(24.dp)) {
@@ -46,8 +51,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
