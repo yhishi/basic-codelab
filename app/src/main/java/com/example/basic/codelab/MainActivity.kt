bcaefae174961e823bcb7fac90f92cb99c2b342c
yhishi <ev422119.s.okayama@gmail.com>
2023-10-29 17:33:13 +0900

    別のCOmposableに切り出す

--- app/src/main/java/com/example/basic/codelab/MainActivity.kt
+++ app/src/main/java/com/example/basic/codelab/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basic.codelab.ui.theme.BasicCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +19,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
+    }
+}
+
+@Composable
+private fun MyApp(modifier: Modifier) {
+    // A surface container using the 'background' color from the theme
+    Surface(modifier = modifier, color = MaterialTheme.colorScheme.primary) {
+        Greeting("Android")
     }
 }
 
@@ -30,7 +37,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.padding(24.dp)
     )
 }
 
