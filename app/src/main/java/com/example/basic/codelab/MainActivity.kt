19607b5e2d0baac2a08ecd9a9a08fb805e93c1be
yhishi <ev422119.s.okayama@gmail.com>
2023-10-30 01:13:45 +0900

    Textにテーマのtextを設定

--- app/src/main/java/com/example/basic/codelab/MainActivity.kt
+++ app/src/main/java/com/example/basic/codelab/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basic.codelab.ui.theme.BasicCodelabTheme
@@ -88,7 +89,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(onClick = { expanded.value = !expanded.value }) {
                 Text(if (expanded.value) "Show less" else "Show more")
